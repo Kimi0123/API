ac65383cacda2bc0b7cfb7e59c6451adcca1fafd
Kimi0123 <maharjannirvan01@gmail.com>
2026-04-23 09:45:08 +0545

    updated

---
+++ typeScript/app.ts
@@ -0,0 +1,16 @@
+import express, {Application, Request, Response} from "express";
+
+const app: Application = express();
+const PORT: number = 8089;
+
+app.get("/", (req: Request, res: Response) => {//callback function
+  res.send("Hello Typescript with Express!");
+});
+
+app.listen(
+  PORT, //start backend in this port
+  () => { //callback function
+    console.log(`Server is running on port ${PORT}`); //backtick
+  }
+);
+
